90f7efaeb2e315fde5e957013f4e15281937e9f8
mohammad mahdi <mo.mahdi.ka@gmail.com>
2024-02-04 22:36:44 +0330

    Get Posts Successfully

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -1,4 +1,22 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_POSTS = gql`
+  query Posts {
+    posts {
+      createdAt
+      datePublished
+      id
+      publishedAt
+      slug
+      title
+      updatedAt
+    }
+  }
+`;
+
 const Blogs = () => {
+  const res = useQuery(GET_POSTS);
+  console.log(res);
   return (
     <div>
       <h1>Blogs Page</h1>
