60e62bd57505e9381354b352885b85f2f0d3860e
mohammad mahdi <mo.mahdi.ka@gmail.com>
2024-02-04 22:24:38 +0330

    Create ApolloClient + Conect To GQL

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './global.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./global.css";
+// ! Import GraphQL Provider
+import GraphQL from "./graphql/GraphQL.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <GraphQL>
+      <App />
+    </GraphQL>
+  </React.StrictMode>
+);
