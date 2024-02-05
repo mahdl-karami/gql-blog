2b23ac423ce1b164bc0059333415d3ebca199e7c
mohammad mahdi <mo.mahdi.ka@gmail.com>
2024-02-05 12:39:06 +0330

    Error Handling DeBug

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,7 +3,7 @@ const Error = ({error}) => {
   return (
     <>
         <h2>Somthing is wrong</h2>
-        <p>{error ? error : "Unknow Error ..."}</p>
+        <p>{error ? error.message : "Unknow Error ..."}</p>
     </>
   )
 }
