7d1e61aa1b1ed9aca679edc7e01818b30620ca81
mohammad mahdi <mo.mahdi.ka@gmail.com>
2024-02-07 20:13:43 +0330

    Add .env File

--- src/graphql/GraphQL.jsx
+++ src/graphql/GraphQL.jsx
@@ -1,7 +1,7 @@
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/cls68vjrz0j2o01w3a5d00jr2/master",
+  uri: import.meta.env.VITE_GQL_URI,
   cache: new InMemoryCache(),
 });
 
