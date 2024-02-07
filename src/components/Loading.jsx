c821e86b49b0bab7cded045e67f60b28a8502150
mohammad mahdi <mo.mahdi.ka@gmail.com>
2024-02-07 22:29:08 +0330

    Edit Loading Progress

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import LinearProgress from "@mui/material/LinearProgress";
 
 const Loading = () => {
   return (
-    <Box sx={{ width: "100%", position: "fixed", top: "0" }}>
+    <Box sx={{ width: "100%", position: "fixed", top: "4rem" , left : "0"}}>
       <LinearProgress />
     </Box>
   );
